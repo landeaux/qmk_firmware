c59d757317b877853600d457d53ee52563d36573
lando <pluckedowl@gmail.com>
2022-06-19 17:08:44 -0700

    improve tap/hold behavior for home-row mods

--- keyboards/crkbd/keymaps/landeaux/config.h
+++ keyboards/crkbd/keymaps/landeaux/config.h
@@ -29,8 +29,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 // #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
+#define BILATERAL_COMBINATIONS
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
